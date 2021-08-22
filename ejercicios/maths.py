9f0100ba7d4043a37844d23e984b0b70d3dd6a8c
Bryan Imbaquingo <bryan_3871@hotmail.com>
2021-08-22 12:17:25 -0500

    maths

---
+++ ejercicios/maths.py
@@ -0,0 +1,20 @@
+import math
+
+# aritmetic operation
+print(10 / 3, "/")
+print(10 // 3, "//")
+print(10 % 3, "%")
+print(10 ** 3, "**")
+
+x = 10
+x += 3
+x = (2 + 3) * 10 - 3
+print(x)
+
+# Math functions
+x = 2.9
+
+print(math.floor(x))
+print(round(x))
+# absolute
+print(abs(-x))
